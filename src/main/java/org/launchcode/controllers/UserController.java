87a8917776217de7d9d391c20cec6cd7d47e3e02
Michael Porter <mb_porter@outlook.com>
2018-06-18 19:59:51 -0500

    finished 3.12 (studio part 1)

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -2,6 +2,8 @@ package org.launchcode.controllers;
 
 
 import org.launchcode.models.User;
+import org.launchcode.models.data.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -10,10 +12,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
+
+    @Autowired
+    private UserDao userDao;
 
     @RequestMapping(value = "add")
     public String add(Model model) {
@@ -24,11 +30,20 @@ public class UserController {
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
     public String add(Model model, @Valid @ModelAttribute User user, Errors errors, String verify){
-        if (verify.equals(user.getPassword()) && errors.hasErrors()==false) {
+        List<User> sameName = userDao.findByusername(user.getUsername());
+        if (verify.equals(user.getPassword()) && errors.hasErrors()==false && sameName.isEmpty()) {
             model.addAttribute("user", user);
+            userDao.save(user);
             return "user/index";
         }
         else{
+            if (!sameName.isEmpty()) {
+                model.addAttribute("title", "User Signup");
+                model.addAttribute("message", "Passwords do not match");
+                model.addAttribute("duplicate", "That username is taken!");
+                return "user/add";
+            }
+
             model.addAttribute("title", "User Signup");
             model.addAttribute("message", "Passwords do not match");
             return "user/add";
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -2,10 +2,19 @@ package org.launchcode.models;
 
 import org.hibernate.validator.constraints.Email;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
+    private int id;
 
     @NotNull
     @Size(min=5, max=15)
